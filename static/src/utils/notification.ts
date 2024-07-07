9a183be3045aed46fde6a15e2c04e92f7b8a1869
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-07-07 14:51:57 +0100

    chore: move toast

---
+++ static/src/utils/notification.ts
@@ -0,0 +1,34 @@
+
+export function toastNotification(message: string, timeout?: number) {
+    const toast = document.createElement("div");
+    toast.textContent = message;
+
+    const style = `
+        position: fixed;
+        bottom: 0;
+        right: 0;
+        background-color: #333;
+        color: white;
+        padding: 1rem;
+        border-radius: 0.5rem;
+        margin: 1rem;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        `
+    toast.style.cssText = style;
+
+    document.body.appendChild(toast);
+
+    function killToast() {
+        document.body.removeChild(toast);
+    }
+
+    if (!timeout) {
+        return killToast;
+    }
+
+    setTimeout(() => {
+        killToast();
+    }, timeout);
+
+    return killToast;
+}
