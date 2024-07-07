05fc29ba8fc802989c6a9a2fced459b75a621d2a
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-07-07 16:42:51 +0100

    chore: turbopack, env vars, error pages

--- next-ui/next.config.js
+++ next-ui/next.config.js
@@ -1,4 +1,16 @@
+const { execSync } = require("child_process");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    tsconfigPath: "./tsconfig.json",
+  },
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
+    SALT: process.env.SALT,
+    commitHash: execSync(`git rev-parse --short HEAD`).toString().trim(),
+  },
+};
 
 module.exports = nextConfig;
