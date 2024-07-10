eea10eedb7ea6d727961e578d4ae349b2db8804f
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-07-10 20:07:13 +0100

    chore: v basic funds ui

--- app/(screens)/account/components/account.tsx
+++ app/(screens)/account/components/account.tsx
@@ -147,7 +147,6 @@ function GroupOptionsSelect({ options, cat }: { options: string[]; cat: string }
 }
 
 function DropDownOptions({ options, cat }: { options: string[] | number; cat: string }) {
-  const [selected, setSelected] = useState<(string[] | number) | null>();
   const opts = typeof options === "number" ? Array.from({ length: options }, (_, i) => i + 1) : options;
   return (
     <Select>
@@ -323,5 +322,40 @@ function BreadCrumbs({ steps }: { steps: string[] }) {
  * As well as stats on earnings, etc.
  */
 function Earnings() {
-  return <p></p>;
+  return (
+    <div className="flex flex-row gap-4 mt-2 h-full">
+      <Card className="bg-[#3333] border-[#333] text-white">
+        <div className="flex flex-col gap-4">
+          <p className="text-lg font-semibold self-center">Transfer</p>
+
+          <div className="grid grid-cols-2 gap-4">
+            <Select>
+              <SelectTrigger className="w-[180px] data-[state=open]:bg-[#000] bg-[#333] hover:bg-[#000] text-white font-bold px-4 rounded left-0 data-[state=closed]:bg-[#333] data-[state=open]:text-white data-[state=open]:font-bold data-[state=open]:px-4 data-[state=open]:rounded">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent className="opacity-100 bg-gray-500">
+                <SelectItem value="DAI">DAI</SelectItem>
+                <SelectItem value="ETH">ETH</SelectItem>
+                <SelectItem value="WXdai">WXdai</SelectItem>
+              </SelectContent>
+            </Select>
+            <input type="text" placeholder="Amount" className="p-2 rounded-md bg-[#000]" />
+            <input type="text" placeholder="Address" className="p-2 rounded-md bg-[#000]" />
+            <Button className="bg-[#3333] hover:bg-[#000] text-white font-bold px-4 rounded left-0">Send</Button>
+          </div>
+        </div>
+      </Card>
+      <Card className="bg-[#3333] border-[#333] text-white">
+        <div className="flex flex-col gap-4">
+          <p className="text-lg font-semibold">Withdraw</p>
+          <div className="grid grid-cols-2 gap-4">
+            {/* USD Balance */}
+            <p className="text-lg">Balance: $356.76</p>
+            <input type="text" placeholder="Amount" className="p-2 rounded-md bg-[#000]" />
+            <Button className="bg-[#3333] hover:bg-[#000] col-span-2 text-white font-bold px-4 rounded left-0">Withdraw</Button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
 }
